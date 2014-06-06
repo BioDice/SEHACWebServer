b822efc8da199e25c04a6bb9a7f88f4ec9e339b3
leon van woezik <leonvanwoezik@hotmail.com>
2014-06-06 11:50:38 +0200

    settings

--- SeHacWebServer/Properties/Settings.Designer.cs
+++ SeHacWebServer/Properties/Settings.Designer.cs
@@ -22,27 +22,5 @@ namespace SeHacWebServer.Properties {
                 return defaultInstance;
             }
         }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\v11.0;AttachDbFilename=|DataDirectory|\\Database\\LoginDataba" +
-            "se.mdf;Integrated Security=True")]
-        public string LoginDatabaseConnectionString {
-            get {
-                return ((string)(this["LoginDatabaseConnectionString"]));
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\v11.0;AttachDbFilename=|DataDirectory|\\Database\\UserLogin.m" +
-            "df;Integrated Security=True")]
-        public string UserLoginConnectionString {
-            get {
-                return ((string)(this["UserLoginConnectionString"]));
-            }
-        }
     }
 }
